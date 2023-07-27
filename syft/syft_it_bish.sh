dd9361d0009c73aa5d08e687d56586a5bc21088b
Derek Verbeeck <dverbeeck@gmail.com>
2023-07-27 16:30:32 -0400

    added minor commentary...

--- syft/syft_it_bish.sh
+++ syft/syft_it_bish.sh
@@ -2,7 +2,8 @@
 
 projectName='mope'
 
-dotheThings() { sleep 5; }
+dotheThings() { sleep 5; } #eventually plan to have this do some post processing - maybe break out rpm dep graph seperately idfk
+
 for outputType in cyclonedx-json template cyclonedx-xml spdx-tag-value #syft-json cyclonedx-xml github-json spdx-tag-value spdx-json syft-table sytf-text template
 	do 
 		syft packages dir:/ --name $projectName -o $outputType > $projectName-$outputType-$(date +%s%3N) && dotheThings
